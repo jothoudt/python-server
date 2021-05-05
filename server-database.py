35b5de1823a7ea4a4581ebf95fb984beaba2af6f
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-05-05 09:52:19 -0500

    start python server repo

---
+++ server-database.py
@@ -0,0 +1,16 @@
+import flask
+import pyscopg2
+from flask import request, jsonify
+from psycopg2.extras import RealDictCursor
+
+app= flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+connection = pyscopg2.connect(
+    host="127.0.0.1",
+    port="5432",
+    database="python_books"
+)
+
+@app.route('/api/pets', methods=['GET'])
+  def list_pets():
